369d8cd7303859745a35443c0f0307eb8bcde13b
jianliang <jianliangw1@gmail.com>
2024-06-01 23:14:13 +0800

    add gradio

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from gradio_client import Client
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+
+    client = Client("Dzkaka/ChatTTS")
+    result = client.predict(
+        text="四川美食确实以辣闻名，但也有不辣的选择。比如甜水面、赖汤圆、蛋烘糕、叶儿粑等，这些小吃口味温和，甜而不腻，也很受欢迎。",
+        temperature=0.3,
+        top_P=0.7,
+        top_K=20,
+        audio_seed_input=42,
+        text_seed_input=42,
+        refine_text_flag=True,
+        api_name="/generate_audio"
+    )
+    print(result)
+
+    return "<p>Hello, World!</p>"
